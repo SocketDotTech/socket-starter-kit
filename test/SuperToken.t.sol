b09e74f9a824aff2e331d959def015d268eca783
Rookmate <14072042+rookmate@users.noreply.github.com>
2024-12-26 21:17:54 +0000

    Add Simple MockERC20

--- test/SuperToken.t.sol
+++ test/SuperToken.t.sol
@@ -6,7 +6,13 @@ import "socket-protocol/test/AuctionHouse.sol";
 import {SuperTokenDeployer} from "../src/superToken/SuperTokenDeployer.sol";
 import {SuperTokenAppGateway} from "../src/superToken/SuperTokenAppGateway.sol";
 
+import {MockERC20} from "./mocks/MockERC20.sol";
+
 contract SuperTokenTest is AuctionHouseTest {
+    uint256 srcAmount = 0.01 ether;
+
+    MockERC20 public token;
+
     struct AppContracts {
         SuperTokenAppGateway superTokenApp;
         SuperTokenDeployer superTokenDeployer;
@@ -15,7 +21,6 @@ contract SuperTokenTest is AuctionHouseTest {
     }
 
     AppContracts appContracts;
-    uint256 srcAmount = 0.01 ether;
     SuperTokenAppGateway.UserOrder userOrder;
 
     event BatchCancelled(bytes32 indexed asyncId);
@@ -24,15 +29,19 @@ contract SuperTokenTest is AuctionHouseTest {
     function setUp() public {
         // set up infrastructure
         setUpAuctionHouse();
-        // TODO: Will need to deploy an initial ERC20
+
+        // Deploy mock ERC20
+        token = new MockERC20("Mock Token", "MCK", 18);
+        token.mint(address(this), 1000 * 10 ** 18);
+
         // Deploy Deployer and AppGateway
         deploySuperTokenApp();
     }
 
     function deploySuperTokenApp() internal {
         SuperTokenDeployer superTokenDeployer = new SuperTokenDeployer(
-            uint32(0), // TODO: Add some chain address like ArbSepolia
-            address(0), // TODO: Add token address deployed on setUp
+            optChainSlug,
+            address(token),
             owner,
             "SUPER TOKEN",
             "SUPER",
@@ -40,6 +49,7 @@ contract SuperTokenTest is AuctionHouseTest {
             address(addressResolver),
             createFeesData(maxFees)
         );
+
         SuperTokenAppGateway superTokenApp =
             new SuperTokenAppGateway(address(addressResolver), address(superTokenDeployer), createFeesData(maxFees));
 
@@ -49,5 +59,14 @@ contract SuperTokenTest is AuctionHouseTest {
             superToken: superTokenDeployer.superToken(),
             vault: superTokenDeployer.vault()
         });
+    }
+
+    ////////////////////////
+    //  HELPER FUNCTIONS  //
+    ////////////////////////
+
+    // To mint tokens for testing to any user if needed
+    function mintTokens(address to, uint256 amount) internal {
+        token.mint(to, amount);
     }
 }
