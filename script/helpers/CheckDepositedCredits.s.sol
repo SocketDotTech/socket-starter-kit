fc0d1e6156a935348ac7f46ee2b20f5897f0b30c
Rookmate <14072042+rookmate@users.noreply.github.com>
2025-06-04 14:26:29 +0100

    chore: update check credits script

--- script/helpers/CheckDepositedCredits.s.sol
+++ script/helpers/CheckDepositedCredits.s.sol
@@ -5,18 +5,21 @@ import {Script} from "forge-std/Script.sol";
 import {console} from "forge-std/console.sol";
 import {FeesManager} from "socket-protocol/contracts/evmx/fees/FeesManager.sol";
 
-contract CheckDepositedFees is Script {
+contract CheckAvailableCredits is Script {
     function run() external {
-        vm.createSelectFork(vm.envString("EVMX_RPC"));
         FeesManager feesManager = FeesManager(payable(vm.envAddress("FEES_MANAGER")));
-        address appGateway = vm.envAddress("APP_GATEWAY");
 
-        (uint256 deposited, uint256 blocked) = feesManager.userCredits(appGateway);
-        console.log("AppGateway:", appGateway);
-        console.log("Total balance of available fees for this AppGateway: %s", deposited);
-        console.log("Fees being locked due to existing transactions: %s", blocked);
+        uint256 privateKey = vm.envUint("PRIVATE_KEY");
+        address sender = vm.addr(privateKey);
+        console.log("Sender address:", sender);
 
-        uint256 availableFees = feesManager.getAvailableCredits(appGateway);
-        console.log("Fees available to be spent on transactions: %s", availableFees);
+        vm.createSelectFork(vm.envString("EVMX_RPC"));
+
+        (uint256 deposited, uint256 blocked) = feesManager.userCredits(sender);
+        console.log("Total balance of available credits for this address: %s", deposited);
+        console.log("Credits being locked due to existing transactions: %s", blocked);
+
+        uint256 availableFees = feesManager.getAvailableCredits(sender);
+        console.log("Credits available to be spent on transactions: %s", availableFees);
     }
 }
